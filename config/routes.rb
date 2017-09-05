2e1e337680b868a4245de2a2ca7784b042f781e4
ENGEZIELLITHON <engeziellithonvieira@gmail.com>
2017-09-05 08:57:40 -0300

    acesso artigos para pessoas com login ativo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     
-  resources :articles
+    resources :articles
     devise_for :users, :controllers => { registrations: 'registrations' }
     
     root 'pages#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170904175714) do
+ActiveRecord::Schema.define(version: 20170905114457) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
