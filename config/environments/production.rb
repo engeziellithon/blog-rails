ed8306886a641b51ee5e4621b738a199239f154e
ENGEZIELLITHON <engeziellithonvieira@gmail.com>
2017-09-09 12:30:04 -0300

    preparando ckeditor imagens para heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,6 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -88,4 +87,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.assets.compile = true
+  config.assets.precompile += Ckeditor.assets
+  config.assets.precompile += %w(ckeditor/* )
+  config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 end
