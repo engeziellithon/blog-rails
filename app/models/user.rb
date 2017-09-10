5479783d0f8df37968f8d8a526f7de61eeaa0ddc
ENGEZIELLITHON <engeziellithonvieira@gmail.com>
2017-09-10 09:23:33 -0300

    upload em https no cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,24 @@ class User < ApplicationRecord
   
   has_many :articles 
   has_many :comments 
-
+  
   has_attached_file :avatar,
   :storage => :cloudinary,
-  :path => ':id/:style/:filename'
+  :path => ':id/:style/:filename',
+  :styles => { :avatar => '200x200>' },
+  :cloudinary_url_options => {
+      :default => {
+          :secure => true
+      },
+      :styles => {
+          :avatar => {
+              :quality => 10,
+              :transformation => [ { :angle => 20 } ]
+          }
+      }
+  }
+
+
   validates_attachment :avatar, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   
   end
